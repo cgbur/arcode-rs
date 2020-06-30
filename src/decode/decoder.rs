a7261add56f02d84b04dfff3503d11f1c001a412
Chris Burgess <9002722+cgburgess@users.noreply.github.com>
2020-06-30 18:24:17 -0400

    2 spaces indent

--- src/decode/decoder.rs
+++ src/decode/decoder.rs
@@ -5,121 +5,121 @@ use crate::util::range::Range;
 use crate::util::source_model::SourceModel;
 
 pub struct ArithmeticDecoder {
-    range: Range,
-    precision: u64,
-    first_time: bool,
-    input_buffer: u64,
-    finished: bool,
+  range: Range,
+  precision: u64,
+  first_time: bool,
+  input_buffer: u64,
+  finished: bool,
 }
 
 impl ArithmeticDecoder {
-    /// # Arguments
-    /// `precision` is the [bit precision](https://en.wikipedia.org/wiki/Arithmetic_coding#Precision_and_renormalization)
-    /// that the decoder should use. If the
-    /// precision is too low than symbols will not be able to be differentiated.
-    pub fn new(precision: u64) -> Self {
-        Self {
-            range: Range::new(precision),
-            precision,
-            first_time: true,
-            input_buffer: 0,
-            finished: false,
-        }
+  /// # Arguments
+  /// `precision` is the [bit precision](https://en.wikipedia.org/wiki/Arithmetic_coding#Precision_and_renormalization)
+  /// that the decoder should use. If the
+  /// precision is too low than symbols will not be able to be differentiated.
+  pub fn new(precision: u64) -> Self {
+    Self {
+      range: Range::new(precision),
+      precision,
+      first_time: true,
+      input_buffer: 0,
+      finished: false,
+    }
+  }
+
+  pub fn decode<R: Read, B: Bit>(
+    &mut self, source_model:
+    &SourceModel, bit_source:
+    &mut BitReader<R, B>) -> Result<u32, Error> {
+    if self.first_time {
+      for _i in 0..self.precision {
+        self.input_buffer = (self.input_buffer << 1) | self.bit(bit_source)?;
+      }
+      self.first_time = false;
     }
 
-    pub fn decode<R: Read, B: Bit>(
-        &mut self, source_model:
-        &SourceModel, bit_source:
-        &mut BitReader<R, B>) -> Result<u32, Error> {
-        if self.first_time {
-            for _i in 0..self.precision {
-                self.input_buffer = (self.input_buffer << 1) | self.bit(bit_source)?;
-            }
-            self.first_time = false;
-        }
-
-        let symbol: u32;
-        let mut low_high: (u64, u64);
-        let mut sym_idx_low_high = (0, source_model.len());
-        loop {
-            let sym_idx_mid = (sym_idx_low_high.0 + sym_idx_low_high.1) / 2;
-            low_high = self.range.calculate_range(sym_idx_mid, source_model);
-            if low_high.0 <= self.input_buffer && self.input_buffer < low_high.1 {
-                symbol = sym_idx_mid;
-                break;
-            } else if self.input_buffer >= low_high.1 {
-                sym_idx_low_high.0 = sym_idx_mid + 1;
-            } else {
-                sym_idx_low_high.1 = sym_idx_mid - 1;
-            }
-        }
-        if symbol == source_model.eof() {
-            self.set_finished();
-            return Ok(symbol);
-        }
-
-        self.range.update_range(low_high);
-
-        while self.range.in_bottom_half() || self.range.in_upper_half() {
-            if self.range.in_bottom_half() {
-                self.range.scale_bottom_half();
-                self.input_buffer = (2 * self.input_buffer) | self.bit(bit_source)?;
-            } else if self.range.in_upper_half() {
-                self.range.scale_upper_half();
-                self.input_buffer = (2 * (self.input_buffer - self.range.half())) | self.bit(bit_source)?;
-            }
-        }
-
-        while self.range.in_middle_half() {
-            self.range.scale_middle_half();
-            self.input_buffer = (2 * (self.input_buffer - self.range.quarter())) | self.bit(bit_source)?;
-        }
-        Ok(symbol)
+    let symbol: u32;
+    let mut low_high: (u64, u64);
+    let mut sym_idx_low_high = (0, source_model.len());
+    loop {
+      let sym_idx_mid = (sym_idx_low_high.0 + sym_idx_low_high.1) / 2;
+      low_high = self.range.calculate_range(sym_idx_mid, source_model);
+      if low_high.0 <= self.input_buffer && self.input_buffer < low_high.1 {
+        symbol = sym_idx_mid;
+        break;
+      } else if self.input_buffer >= low_high.1 {
+        sym_idx_low_high.0 = sym_idx_mid + 1;
+      } else {
+        sym_idx_low_high.1 = sym_idx_mid - 1;
+      }
+    }
+    if symbol == source_model.eof() {
+      self.set_finished();
+      return Ok(symbol);
     }
 
+    self.range.update_range(low_high);
 
-    fn bit<R: Read, B: Bit>(&mut self, source: &mut BitReader<R, B>)
-                            -> Result<u64, Error> {
-        match source.read_bit() {
-            Ok(res) => Ok(res as u64),
-            Err(_e) => {
-                if self.precision == 0 {
-                    return Err(Error::new(ErrorKind::UnexpectedEof,
-                                          "EOF has been read $PRECISION times and \n\
+    while self.range.in_bottom_half() || self.range.in_upper_half() {
+      if self.range.in_bottom_half() {
+        self.range.scale_bottom_half();
+        self.input_buffer = (2 * self.input_buffer) | self.bit(bit_source)?;
+      } else if self.range.in_upper_half() {
+        self.range.scale_upper_half();
+        self.input_buffer = (2 * (self.input_buffer - self.range.half())) | self.bit(bit_source)?;
+      }
+    }
+
+    while self.range.in_middle_half() {
+      self.range.scale_middle_half();
+      self.input_buffer = (2 * (self.input_buffer - self.range.quarter())) | self.bit(bit_source)?;
+    }
+    Ok(symbol)
+  }
+
+
+  fn bit<R: Read, B: Bit>(&mut self, source: &mut BitReader<R, B>)
+                          -> Result<u64, Error> {
+    match source.read_bit() {
+      Ok(res) => Ok(res as u64),
+      Err(_e) => {
+        if self.precision == 0 {
+          return Err(Error::new(ErrorKind::UnexpectedEof,
+                                "EOF has been read $PRECISION times and \n\
                                           EOF symbol has not been decoded.\n\
                                            Did you forget to encode the EOF symbol?"));
-                }
-                self.precision -= 1;
-                Ok(0)
-            }
         }
+        self.precision -= 1;
+        Ok(0)
+      }
     }
-    pub fn set_finished(&mut self) {
-        self.finished = true;
-    }
-    pub fn finished(&self) -> bool { self.finished }
+  }
+  pub fn set_finished(&mut self) {
+    self.finished = true;
+  }
+  pub fn finished(&self) -> bool { self.finished }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::io::Cursor;
-    use crate::util::source_model::SourceModel;
-    use bitbit::{BitReader, MSB};
-    use crate::decode::decoder::ArithmeticDecoder;
+  use std::io::Cursor;
+  use crate::util::source_model::SourceModel;
+  use bitbit::{BitReader, MSB};
+  use crate::decode::decoder::ArithmeticDecoder;
 
-    #[test]
-    fn e2e() {
-        let input = Cursor::new(vec![184, 96, 208]);
-        let mut source_model = SourceModel::new_eof(10, 9);
-        let mut output = Vec::new();
-        let mut in_reader: BitReader<_, MSB> = BitReader::new(input);
+  #[test]
+  fn e2e() {
+    let input = Cursor::new(vec![184, 96, 208]);
+    let mut source_model = SourceModel::new_eof(10, 9);
+    let mut output = Vec::new();
+    let mut in_reader: BitReader<_, MSB> = BitReader::new(input);
 
-        let mut decoder = ArithmeticDecoder::new(30);
-        while !decoder.finished() {
-            let sym = decoder.decode(&source_model, &mut in_reader).unwrap();
-            source_model.update_symbol(sym);
-            if sym != source_model.eof() { output.push(sym) };
-        }
-        assert_eq!(output, &[7, 2, 2, 2, 7]);
+    let mut decoder = ArithmeticDecoder::new(30);
+    while !decoder.finished() {
+      let sym = decoder.decode(&source_model, &mut in_reader).unwrap();
+      source_model.update_symbol(sym);
+      if sym != source_model.eof() { output.push(sym) };
     }
+    assert_eq!(output, &[7, 2, 2, 2, 7]);
+  }
 }
--- src/util/source_model.rs
+++ src/util/source_model.rs
@@ -1,172 +1,170 @@
 pub struct SourceModel {
-    counts: Vec<u32>,
-    fenwick_counts: Vec<u32>,
-    total_count: u32,
-    eof: u32,
+  counts: Vec<u32>,
+  fenwick_counts: Vec<u32>,
+  total_count: u32,
+  eof: u32,
 }
 
 use fenwick::array::{update, prefix_sum};
 
 impl SourceModel {
-    pub fn new_eof(symbols_count: u32, eof_symbol: u32) -> Self {
-        let count_vector = vec![1; symbols_count as usize];
-        let mut fenwick_counts = vec![0u32; count_vector.len()];
+  pub fn new_eof(symbols_count: u32, eof_symbol: u32) -> Self {
+    let count_vector = vec![1; symbols_count as usize];
+    let mut fenwick_counts = vec![0u32; count_vector.len()];
 
-        for i in 0..count_vector.len() {
-            update(&mut fenwick_counts, i, 1);
-        }
-
-        Self {
-            total_count: symbols_count as u32,
-            eof: eof_symbol,
-            counts: count_vector,
-            fenwick_counts,
-        }
+    for i in 0..count_vector.len() {
+      update(&mut fenwick_counts, i, 1);
     }
 
-    pub fn new(symbols_count: u32) -> Self {
-        Self::new_eof(symbols_count, symbols_count + 1)
+    Self {
+      total_count: symbols_count as u32,
+      eof: eof_symbol,
+      counts: count_vector,
+      fenwick_counts,
+    }
+  }
+
+  pub fn new(symbols_count: u32) -> Self {
+    Self::new_eof(symbols_count, symbols_count + 1)
+  }
+
+  pub fn new_binary() -> Self {
+    Self {
+      counts: vec![1, 1],
+      fenwick_counts: vec![1, 2],
+      total_count: 2,
+      eof: 3,
+    }
+  }
+
+  pub fn from_counts(counts: Vec<u32>, eof: u32) -> Self {
+    let mut fenwick_counts = vec![0u32; counts.len()];
+
+    for i in 0..counts.len() {
+      update(&mut fenwick_counts, i, counts[i]);
     }
 
-    pub fn new_binary() -> Self {
-        Self {
-            counts: vec![1, 1],
-            fenwick_counts: vec![1, 2],
-            total_count: 2,
-            eof: 3,
-        }
+    let total_count = counts.iter().sum();
+    Self {
+      counts,
+      fenwick_counts,
+      total_count,
+      eof,
     }
+  }
 
-    pub fn from_counts(counts: Vec<u32>, eof: u32) -> Self {
+  pub fn update_symbol(&mut self, symbol: u32) {
+    self.total_count += 1;
+    self.counts[symbol as usize] += 1;
+    update(&mut self.fenwick_counts, symbol as usize, 1);
+  }
 
-        let mut fenwick_counts = vec![0u32; counts.len()];
+  pub fn len(&self) -> u32 {
+    self.counts.len() as u32
+  }
 
-        for i in 0..counts.len() {
-            update(&mut fenwick_counts, i, counts[i]);
-        }
+  pub fn high(&self, index: u32) -> f64 {
+    let high: u32 = fenwick::array::prefix_sum(&self.fenwick_counts, index as usize);
+    high as f64 / self.total_count as f64
+  }
 
-        let total_count = counts.iter().sum();
-        Self {
-            counts,
-            fenwick_counts,
-            total_count,
-            eof
-        }
-    }
+  pub fn low(&self, index: u32) -> f64 {
+    let low: u32 = fenwick::array::prefix_sum(&self.fenwick_counts, index as usize) - self.counts[index as usize];
+    low as f64 / self.total_count as f64
+  }
 
-    pub fn update_symbol(&mut self, symbol: u32) {
-        self.total_count += 1;
-        self.counts[symbol as usize] += 1;
-        update(&mut self.fenwick_counts, symbol as usize, 1);
-    }
+  pub fn probability(&self, symbol: u32) -> (f64, f64) {
+    let total = self.total_count as f64;
 
-    pub fn len(&self) -> u32 {
-        self.counts.len() as u32
-    }
+    let high = prefix_sum(&self.fenwick_counts, symbol as usize);
+    let low = high - self.counts[symbol as usize];
+    (low as f64 / total, high as f64 / total)
+  }
 
-    pub fn high(&self, index: u32) -> f64 {
-        let high: u32 = fenwick::array::prefix_sum(&self.fenwick_counts, index as usize);
-        high as f64 / self.total_count as f64
-    }
+  fn generate_symbol_vec(num_symbols: u32) -> Vec<u32> {
+    (0..num_symbols).collect()
+  }
 
-    pub fn low(&self, index: u32) -> f64 {
-        let low: u32 = fenwick::array::prefix_sum(&self.fenwick_counts, index as usize) - self.counts[index as usize];
-        low as f64 / self.total_count as f64
-    }
-
-    pub fn probability(&self, symbol: u32) -> (f64, f64) {
-        let total = self.total_count as f64;
-
-        let high = prefix_sum(&self.fenwick_counts, symbol as usize);
-        let low = high - self.counts[symbol as usize];
-        (low as f64 / total, high as f64 / total)
-    }
-
-    fn generate_symbol_vec(num_symbols: u32) -> Vec<u32> {
-        (0..num_symbols).collect()
-    }
-
-    pub fn eof(&self) -> u32 {
-        self.eof
-    }
+  pub fn eof(&self) -> u32 {
+    self.eof
+  }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::util::source_model::SourceModel;
+  use crate::util::source_model::SourceModel;
 
-    #[test]
-    fn constructor() {
-        let model = SourceModel::new_eof(4, 3);
-        assert_eq!(3, model.eof());
-        assert_eq!(model.probability(0), (0.0, 0.25));
-        assert_eq!(model.probability(1), (0.25, 0.5));
-        assert_eq!(model.probability(2), (0.5, 0.75));
-        assert_eq!(model.probability(3), (0.75, 1.0));
-    }
+  #[test]
+  fn constructor() {
+    let model = SourceModel::new_eof(4, 3);
+    assert_eq!(3, model.eof());
+    assert_eq!(model.probability(0), (0.0, 0.25));
+    assert_eq!(model.probability(1), (0.25, 0.5));
+    assert_eq!(model.probability(2), (0.5, 0.75));
+    assert_eq!(model.probability(3), (0.75, 1.0));
+  }
 
-    #[test]
-    fn constructor_binary() {
-        let binary = SourceModel::new_binary();
-        let model = SourceModel::new(2);
-        assert_eq!(binary.eof(), model.eof());
-        assert_eq!(binary.probability(0), model.probability(0));
-        assert_eq!(binary.probability(1), model.probability(1));
-    }
+  #[test]
+  fn constructor_binary() {
+    let binary = SourceModel::new_binary();
+    let model = SourceModel::new(2);
+    assert_eq!(binary.eof(), model.eof());
+    assert_eq!(binary.probability(0), model.probability(0));
+    assert_eq!(binary.probability(1), model.probability(1));
+  }
 
-    #[test]
-    fn constructor_from_counts() {
-        let mut model = SourceModel::new_eof(4, 3);
-        let counts_model = SourceModel::from_counts(vec![1;4], 3);
-        assert_eq!(3, model.eof());
-        assert_eq!(model.probability(0), counts_model.probability(0));
-        assert_eq!(model.probability(1), counts_model.probability(1));
-        assert_eq!(model.probability(2), counts_model.probability(2));
-        assert_eq!(model.probability(3), counts_model.probability(3));
+  #[test]
+  fn constructor_from_counts() {
+    let mut model = SourceModel::new_eof(4, 3);
+    let counts_model = SourceModel::from_counts(vec![1; 4], 3);
+    assert_eq!(3, model.eof());
+    assert_eq!(model.probability(0), counts_model.probability(0));
+    assert_eq!(model.probability(1), counts_model.probability(1));
+    assert_eq!(model.probability(2), counts_model.probability(2));
+    assert_eq!(model.probability(3), counts_model.probability(3));
 
-        model.update_symbol(0);
-        model.update_symbol(0);
-        model.update_symbol(0);
-        model.update_symbol(2);
-        model.update_symbol(2);
+    model.update_symbol(0);
+    model.update_symbol(0);
+    model.update_symbol(0);
+    model.update_symbol(2);
+    model.update_symbol(2);
 
-        let counts_model = SourceModel::from_counts(vec![4, 1, 3, 1], 3);
-        assert_eq!(model.probability(0), counts_model.probability(0));
-        assert_eq!(model.probability(1), counts_model.probability(1));
-        assert_eq!(model.probability(2), counts_model.probability(2));
-        assert_eq!(model.probability(3), counts_model.probability(3));
-    }
+    let counts_model = SourceModel::from_counts(vec![4, 1, 3, 1], 3);
+    assert_eq!(model.probability(0), counts_model.probability(0));
+    assert_eq!(model.probability(1), counts_model.probability(1));
+    assert_eq!(model.probability(2), counts_model.probability(2));
+    assert_eq!(model.probability(3), counts_model.probability(3));
+  }
 
-    #[test]
-    fn probability_min() {
-        let model = SourceModel::new_eof(1000, 3);
-        assert_eq!(model.probability(0),
-                   (model.low(0), model.high(0)));
-    }
+  #[test]
+  fn probability_min() {
+    let model = SourceModel::new_eof(1000, 3);
+    assert_eq!(model.probability(0),
+               (model.low(0), model.high(0)));
+  }
 
-    #[test]
-    fn probability_high() {
-        let count = 1_000;
-        let model = SourceModel::new_eof(count + 1, 3);
+  #[test]
+  fn probability_high() {
+    let count = 1_000;
+    let model = SourceModel::new_eof(count + 1, 3);
 
-        assert_eq!(model.probability(count),
-                   (model.low(count), model.high(count)));
-    }
+    assert_eq!(model.probability(count),
+               (model.low(count), model.high(count)));
+  }
 
-    #[test]
-    fn update_symbols() {
-        let mut model = SourceModel::new_eof(4, 3);
-        model.update_symbol(2);
-        model.update_symbol(2);
-        model.update_symbol(2);
-        model.update_symbol(3);
-        model.update_symbol(1);
-        model.update_symbol(3);
+  #[test]
+  fn update_symbols() {
+    let mut model = SourceModel::new_eof(4, 3);
+    model.update_symbol(2);
+    model.update_symbol(2);
+    model.update_symbol(2);
+    model.update_symbol(3);
+    model.update_symbol(1);
+    model.update_symbol(3);
 
-        assert_eq!(model.probability(0), (0.0, 0.1));
-        assert_eq!(model.probability(1), (0.1, 0.3));
-        assert_eq!(model.probability(2), (0.3, 0.7));
-        assert_eq!(model.probability(3), (0.7, 1.0));
-    }
-
+    assert_eq!(model.probability(0), (0.0, 0.1));
+    assert_eq!(model.probability(1), (0.1, 0.3));
+    assert_eq!(model.probability(2), (0.3, 0.7));
+    assert_eq!(model.probability(3), (0.7, 1.0));
+  }
 }
