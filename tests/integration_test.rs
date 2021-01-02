dd5bcb38575809914ab879fe46ee5cad750fe127
chris <cburgess4397@gmail.com>
2021-01-01 23:51:17 -0500

    Update integration_test.rs

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,18 +1,20 @@
+use arcode::bitbit::{BitReader, BitWriter, MSB};
 use arcode::decode::decoder::ArithmeticDecoder;
 use arcode::encode::encoder::ArithmeticEncoder;
 use arcode::util::source_model_builder::{EOFKind, SourceModelBuilder};
-use bitbit::{BitReader, BitWriter, MSB};
 use sherlock::SHERLOCK;
 use std::io::{Cursor, Result};
 
 mod sherlock;
 
+/// Encodes bytes and returns the compressed form
 fn encode(data: &[u8]) -> Result<Vec<u8>> {
   let mut model = SourceModelBuilder::new()
     .num_bits(8)
     .eof(EOFKind::EndAddOne)
     .build();
 
+  // make a stream to collect the compressed data
   let compressed = Cursor::new(vec![]);
   let mut compressed_writer = BitWriter::new(compressed);
 
@@ -22,12 +24,18 @@ fn encode(data: &[u8]) -> Result<Vec<u8>> {
     encoder.encode(sym as u32, &model, &mut compressed_writer)?;
     model.update_symbol(sym as u32);
   }
+
   encoder.encode(model.eof(), &model, &mut compressed_writer)?;
   encoder.finish_encode(&mut compressed_writer)?;
+  compressed_writer.pad_to_byte()?;
 
+  // retrieves the bytes from the writer. This will
+  // be cleaner when bitbit updates. Not necessary if
+  // using files or a stream
   Ok(compressed_writer.get_ref().get_ref().clone())
 }
 
+/// Decompresses the data
 fn decode(data: &[u8]) -> Result<Vec<u8>> {
   let mut model = SourceModelBuilder::new()
     .num_bits(8)
